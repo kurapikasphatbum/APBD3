3065e08c48fa43da8de08e31d055daad7439c76a
kurapikasphatbum <yilmazebru2002@gmail.com>
2024-03-19 11:41:36 +0100

    bos

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Container
+{
+    public double massCargo;
+    public double height;
+    public double massContainer;
+    public double depth;
+    public int ID;
+    public char type;
+}
